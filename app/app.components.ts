c9773191e822a050808c77de4f61161949b4af6c
Chad Durkin <chad@>
2017-03-29 11:16:37 -0700

    template set up

---
+++ app/app.components.ts
@@ -0,0 +1,16 @@
+import { Component }  from '@angular/core';
+import { Hero }       from './hero';
+
+@Component({
+  selector: 'my-app',
+  template: `
+
+  `,
+  styles: ['
+
+  ']
+})
+
+export class AppComponent {
+
+}
